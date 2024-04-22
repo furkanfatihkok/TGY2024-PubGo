e0e2551d2266844737f6da8f5a16e37393ca97d1
furkanfatihkok <furkankok296@gmail.com>
2024-04-22 18:18:44 +0300

    UI olarak onboarding, login ve register screen oluşturuldu.Asset dosyama image ve color oluşturdum.Projemin düzenli durması için klasörler oluşturdum.Model klasörümün altında onboardingmodel'i oluşturdum.

---
+++ bilet-yolcu-odevi/bilet-yolcu-odevi/Controller/Onboarding/Cell/OnboardingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingCollectionViewCell.swift
+//  bilet-yolcu-odevi
+//
+//  Created by FFK on 22.04.2024.
+//
+
+import UIKit
+
+class OnboardingCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ bilet-yolcu-odevi/bilet-yolcu-odevi/Models/OnboardingModel.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingModel.swift
+//  bilet-yolcu-odevi
+//
+//  Created by FFK on 22.04.2024.
+//
+
+import UIKit
+
+struct OnboardingModel {
+    
+    let imageView: UIImage
+    let title: String
+    let description: String
+}
